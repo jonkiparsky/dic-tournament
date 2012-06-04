3a4df69dcedd02357bd26bdfbe5f68451ade562a
TheSheph@gmail.com <TheSheph@gmail.com@005112f6-3693-3f88-cee3-6771d573f91a>
2012-06-04 20:14:44 +0000

    Drafting some statistics we need to gather.

--- trunk/tourney/Statistics.java
+++ trunk/tourney/Statistics.java
@@ -9,6 +9,13 @@ package tourney;
 
 public abstract class Statistics
 {
-
-
+  // just keeping track of what we need
+  
+  private int duration; // in # of moves
+  
+  // the index of the player in the passed List. could be -1 for draw?
+  // how do we handle a draw between 2 people in a 3 man game.
+  private int winner;
+  
+  private int whoWentFirst; // Which player went first?
 }
