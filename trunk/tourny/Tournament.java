64f63e5974511126539a1fc092931d870352b2c2
jon.kiparsky@gmail.com <jon.kiparsky@gmail.com@005112f6-3693-3f88-cee3-6771d573f91a>
2012-06-14 05:30:55 +0000

    Added skeleton permute() method to Tournament in prep for coping with >2 Players at a time. Totally rudimentary, but it should work for two-player games.

--- trunk/tourny/Tournament.java
+++ trunk/tourny/Tournament.java
@@ -6,6 +6,9 @@ import java.util.List;
 import countToN.CountToN;
 
 public class Tournament {
+
+	private static int playersPerGame = 2;
+
 	public static void main(String[] args) {
 		Game g = new CountToN();
 
@@ -15,10 +18,43 @@ public class Tournament {
 
 		List<Move> moves = g.play(players);
 		
-		// get last move
 		Move move = moves.get(moves.size() - 1);
 		
-		// did I win?
 		System.out.println("The winner is "+move.getPlayer().getID());
 	}
+
+
+	/** 
+	*	Purpose: Generate all n-player permutations of the Player list, where n is
+	*	the number of players required per game. Players are to play themselves,
+	*	order is not significant. For a 3-player game, with players a, b, c, d, e
+	*	the set <a,a,a> and <a,a,b> must be generated, but <a,a,b> and <b,a,a>
+	*	must not both be generated. 
+	*/ We will need this presently. It will also need improved. 
+
+	// This only works for two-player games now. 
+   // We should worry about combinations here - consider what happens if we try
+   // to play Chinese Checkers, and 20 people submit Players!
+
+   public ArrayList<ArrayList<Player>> permute (List <Player> players)
+   {
+      ArrayList<ArrayList<Player>> permutations 
+            = new ArrayList<ArrayList<Player>>();
+      
+      ArrayList<Player> copy = new ArrayList<Player>(players);
+
+      for (Player p: players)
+      {
+         for (Player q: copy)
+         {
+            ArrayList<Player> perm = new ArrayList<Player>(playersPerGame);
+            perm.add(p); 
+            perm.add(q);
+            permutations.add(perm);
+         }
+         copy.remove(p);
+      }
+      return permutations;
+   }
+
 }
