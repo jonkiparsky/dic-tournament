74568d0eec4a554c46f6b987e1043c322e11451b
jon.kiparsky <jon.kiparsky@gmail.com>
2012-06-16 05:13:21 +0000

    no need for countToTen any more

--- tourney/TestReflect.java
+++ tourney/TestReflect.java
@@ -1,6 +1,9 @@
 package tourney;
 
+import countToTen.*;
 import countToN.*;
+
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileFilter;
@@ -12,23 +15,54 @@ public class TestReflect
 	{
 		File root = new File ("./src");
 		
-		ArrayList<File> packages= new ArrayList<File>();
+		System.out.println(File.class.getName());
+	
+
 		for (File f: root.listFiles(new FFilter()))
 		{
-			packages.add(f);
-		}
+			ArrayList<Class> games = new ArrayList<Class>();
+			ArrayList<Class> players = new ArrayList<Class>();
+			for (File classFile : f.listFiles(new FFilter()))
+			{	
+				
+				String className = classFile.getName().replace(".java", "");
+				try {
+					Class c = Class.forName(f.getName() + "."+className);
+					if (Game.class.isAssignableFrom(c)){
+						games.add(c);
+					}
+					if (Player.class.isAssignableFrom(c)){
+						players.add(c);
+					}
+					System.out.println("Okay with: " + c.getName()+
+							games.contains(c)+" "+players.contains(c));	
+				}
 			
-		for (File f: packages)
-			System.out.println(f.getName());	
+				catch(Exception e) {  // evil, clean this up
+					e.printStackTrace();
+				}
+			}	
+			for (Class c:games)
+			{
+				System.out.println("Game: "+c.getName());
+			}
+			for (Class c:players)
+			{
+				System.out.println("Player: "+c.getName());
+			}
+		
+		}	
 	}
-
+	
 
 	private class FFilter implements FileFilter
 	{	
-
 		public boolean accept(File f)
 		{
-			return (!f.getName().contains("tourney"));
+			if (f.getName().contains("tourney")) return false;
+			if (f.getName().contains("svn")) return false;
+			return true;
+		
 		}
 	}
 }
