1e4ba79dd9eaa1c6cf72bf35e72db3db6d5d337d
TheSheph@gmail.com <TheSheph@gmail.com@005112f6-3693-3f88-cee3-6771d573f91a>
2012-06-21 02:13:34 +0000

    Added some implementation to Move. I didn't alter its contract, but Games would need to be changed to use the new methods.

--- trunk/tourney/Move.java
+++ trunk/tourney/Move.java
@@ -1,5 +1,6 @@
 package tourney;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -7,8 +8,9 @@ import java.util.HashMap;
  * the state of a game. The relation between GameState and Move is very close
  * and most of the time that forces them to be made in tandem.
  */
-public abstract class Move {
-	
+public abstract class Move
+{
+
 	/** ID of player who made this the move */
 	public static final String PLAYER = "Player";
 
@@ -17,35 +19,63 @@ public abstract class Move {
 
 	/** The turn number this move was made. */
 	public static final String TURN = "Turn";
-	
 
 	/** The player who made this Move. */
 	private Player player;
+	
+	/** The player(s) who won as a result of this move */
+	private ArrayList<Player> winners;
 
 	private HashMap<String, String> annotations = new HashMap<String, String>();
 
 	/** Signs this player's identity to this move. */
-	public void setPlayer(Player player) {
+	public void setPlayer(Player player)
+	{
 		/*
 		 * It should not matter if a Player attempts to use this method because
 		 * the Game class will call this and override whatever he sets.
 		 */
 		this.player = player;
 	}
+	
+	/** Utility method for adding only one player */
+	public void setWinner(Player player) {
+		this.winners = new ArrayList<Player>();
+		this.winners.add(player);
+	}
 
-	public Player getPlayer() {
+	/** Set all the players who tied for first. */
+	public void setWinners(ArrayList<Player> players)
+	{
+		this.winners = players;
+	}
+
+	public Player getPlayer()
+	{
 		return player;
 	}
 
-	public void annotate(String key, String value) {
+	/**
+	 * returns all winners for a game. most of the time this can be
+	 * just size 1, but for draws we'll allow for flexibility
+	 */
+	public ArrayList<Player> getWinners()
+	{
+		return winners;
+	}
+
+	public void annotate(String key, String value)
+	{
 		annotations.put(key, value);
 	}
 
-	public String getAnnotation(String key) {
+	public String getAnnotation(String key)
+	{
 		return annotations.get(key);
 	}
 
-	public boolean isSet(String key) {
+	public boolean isSet(String key)
+	{
 		return annotations.containsKey(key);
 	}
 }
