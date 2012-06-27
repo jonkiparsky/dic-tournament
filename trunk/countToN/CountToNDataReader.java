46b7ef05ed52d53abd9ef3a6698dfbef941a2f48
TheSheph@gmail.com <TheSheph@gmail.com@005112f6-3693-3f88-cee3-6771d573f91a>
2012-06-27 22:08:00 +0000

    A little improvement to r82. Also I fixed my data reader to work with it.

--- trunk/countToN/CountToNDataReader.java
+++ trunk/countToN/CountToNDataReader.java
@@ -3,86 +3,35 @@ package countToN;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import tourney.DataReader;
-import tourney.Match;
 import tourney.MatchResult;
-import tourney.Move;
 import tourney.Player;
 
 /** Handles getting the appropriate Data from a Match of CountToN */
-public class CountToNDataReader implements DataReader
-{
+public class CountToNDataReader implements DataReader {
 	private MatchResult result;
 
-	public CountToNDataReader(MatchResult result)
-	{
+	public CountToNDataReader(MatchResult result) {
 		this.result = result;
 	}
 
-	/**
-	 * Finds the player(s) who won the most frequently. If it is a draw, there
-	 * will be more than one player in winners.
-	 */
-	private void calculateWinners()
-	{
-		List<Player> players = match.getPlayers();
-
-		int[] winnerTally = new int[players.size()];
-
-		for (ArrayList<Move> gameRecord : result)
-		{
-			Move move = gameRecord.get(gameRecord.size() - 1);
-			ArrayList<Player> gameWinners = move.getWinners(); // *** not yet
-																// implemented
-																// in
-																// Move.
-			for (Player winner : gameWinners)
-			{
-				winnerTally[players.indexOf(winner)]++;
-			}
-		}
-
-		int max = 0;
-		for (int i = 0; i < winnerTally.length; i++)
-		{
-			if (winnerTally[i] > max)
-			{
-				max = winnerTally[i];
-			}
-		}
-
-		for (int i = 0; i < winnerTally.length; i++)
-		{
-			if (winnerTally[i] == max)
-			{
-				winners.add(players.get(i));
-			}
-		}
-	}
-
 	@Override
-	public String report()
-	{
+	public String report() {
 		StringBuilder sb = new StringBuilder();
 
-		ArrayList<ArrayList<Move>> gameRecords = match.getMatchResult();
-		List<Player> players = match.getPlayers();
+		List<Player> players = result.getPlayers();
+		List<Player> winners = result.getMatchWinners();
 
-		sb.append(String.format("%d games of CountToN were played between ",
-				gameRecords.size()));
+		sb.append(String.format("%d games of CountToN were played between ", result.size()));
 
 		int playerLength = players.size();
-		for (int i = 0; i < playerLength; i++)
-		{
+		for (int i = 0; i < playerLength; i++) {
 			sb.append(players.get(i).getID());
-			if (i < playerLength - 2)
-			{
+			if (i < playerLength - 2) {
 				sb.append(", ");
-			} else if (i == playerLength - 2)
-			{
+			} else if (i == playerLength - 2) {
 				sb.append(" and ");
 			}
 		}
@@ -90,22 +39,17 @@ public class CountToNDataReader implements DataReader
 		sb.append(".\nThe results are as follows:\n");
 
 		// The Overall Winner(s)
-		if (winners.size() == 1)
-		{
+		if (winners.size() == 1) {
 			sb.append("The player who won the most games was: ");
 			sb.append(winners.get(0).getID());
 			sb.append(".\n");
-		} else
-		{
+		} else {
 			sb.append("There was a draw between players ");
-			for (int i = 0; i < winners.size(); i++)
-			{
+			for (int i = 0; i < winners.size(); i++) {
 				sb.append(winners.get(i).getID());
-				if (i < winners.size() - 2)
-				{
+				if (i < winners.size() - 2) {
 					sb.append(", ");
-				} else if (i == winners.size() - 2)
-				{
+				} else if (i == winners.size() - 2) {
 					sb.append(" and ");
 				}
 			}
@@ -114,21 +58,20 @@ public class CountToNDataReader implements DataReader
 
 		/*
 		 * add additional Data to the report as desired. Can't think of more for
-		 * simple CountToN. Options include:
-		 * - Average/Most/Least Number of Moves per Game (not for CountToN)
-		 * - First blood (in relevant games)
-		 * - # of times an event occurred (Such as getting kinged in checkers)
-		 * - Total winnings (in betting games)
-		 * - Anything that can be polled from annotations.
+		 * simple CountToN. Options include: - Average/Most/Least Number of
+		 * Moves per Game (not for CountToN) - First blood (in relevant games) -
+		 * # of times an event occurred (Such as getting kinged in checkers) -
+		 * Total winnings (in betting games) - Anything that can be polled from
+		 * annotations.
 		 */
 
 		return sb.toString();
 	}
 
 	@Override
-	public void run()
-	{
-		System.out.println("The CountToN interactive data explorer is not available");
+	public void run() {
+		System.out
+				.println("The CountToN interactive data explorer is not available");
 		// We should only expend effort on these for the Games we are going to
 		// actually run for fun.
 		// CountToN, while extremely useful for aiding the design, is a joke for
@@ -136,17 +79,14 @@ public class CountToNDataReader implements DataReader
 	}
 
 	@Override
-	public void write()
-	{
-		String fileName = "CountToNMatch_" + instanceCounter + ".txt";
-		try
-		{
+	public void write() {
+		String fileName = "CountToNMatch.txt";
+		try {
 			BufferedWriter file = new BufferedWriter(new FileWriter(fileName));
 			file.write(report()); // for now if we want to save it. Later we can
 									// decide on format.
 			file.close();
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
--- trunk/tourney/MatchResult.java
+++ trunk/tourney/MatchResult.java
@@ -10,8 +10,10 @@ import java.util.List;
  * GameResults to determine match-wide data for the user.
  */
 public class MatchResult extends ArrayList<GameResult> {
+	private List<Player> players;
+
 	/** A map of players to the number of games they won in the match. */
-	private HashMap<Player, Integer> players;
+	private HashMap<Player, Integer> numberOfWins;
 
 	public MatchResult() {
 		super();
@@ -22,23 +24,54 @@ public class MatchResult extends ArrayList<GameResult> {
 
 		// Add any new players to the list of match-wide players
 		for (Player player : gamePlayers) {
-			if (!players.containsKey(player)) {
-				players.put(player, 0);
+			if (!players.contains(player)) {
+				players.add(player);
+			}
+
+			if (!numberOfWins.containsKey(player)) {
+				numberOfWins.put(player, 0);
 			}
 		}
 
 		List<Player> gameWinners = result.getWinners();
 
 		for (Player winner : gameWinners) {
-			if (players.containsKey(winner)) {
-				int oldValue = players.get(winner);
+			if (numberOfWins.containsKey(winner)) {
+				int oldValue = numberOfWins.get(winner);
 				oldValue++;
-				players.put(winner, oldValue);
+				numberOfWins.put(winner, oldValue);
 			} else {
 				// we don't know who this winner is...
 			}
 		}
 
 		return super.add(result);
+	}
+
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	public List<Player> getMatchWinners() {
+		// Find the highest number in numberOfWins
+		int max = 0;
+		for (Player player : players) {
+			int wins = numberOfWins.get(player);
+			if (wins > max) {
+				max = wins;
+			}
+		}
+
+		// add all players whose value is max to the winner list
+		ArrayList<Player> winners = new ArrayList<Player>();
+
+		for (Player player : players) {
+			int wins = numberOfWins.get(player);
+			if (wins == max) {
+				winners.add(player);
+			}
+		}
+
+		return winners;
 	}
 }
