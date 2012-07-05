7ee21a3e59e2283b35822f06a6036db25ba2d0f1
TheSheph@gmail.com <TheSheph@gmail.com@005112f6-3693-3f88-cee3-6771d573f91a>
2012-07-05 15:00:05 +0000

    javadoc prettying

--- trunk/tourney/TurnBasedGame.java
+++ trunk/tourney/TurnBasedGame.java
@@ -4,24 +4,29 @@ import java.util.List;
 
 /**
  * TurnBasedGame is a game format where players are asked for Moves in turn.
- * Each player is given an indefinite amount of time to come up with his or her
- * Move based on how they see the Board. After every Move all Players are
- * updated to reflect the change.
- * 
+ * Each player is asked to come up with his or her <code>Move</code> based on
+ * how they see the game. After every <code>Move</code> all Players are updated
+ * to reflect the change, and the next player is asked for a <code>Move</code>
+ * again.
+ * <p>
  * Examples include TicTacToe and Chess.
+ * 
+ * @since 0.1
  */
 public abstract class TurnBasedGame extends Game
 {
 	
-	/** The most recent player to make a move. */
+	/** The most recent player to be asked for a move. */
 	protected Player currentPlayer;
 	
-	/** The returned move of the current player. */
+	/** The returned <code>Move</code> of the current player. */
 	protected Move move;
 	
 	/**
-	 * Returns the next player to move. Base implementation simply rotates first
-	 * player in list to end of list.
+	 * Determines the next player in the turn order. Implementations should
+	 * change this if they need anything but a circular turn order.
+	 * 
+	 * @return The <code>Player</code> to get the next move from.
 	 */
 	protected Player nextPlayer()
 	{
@@ -31,9 +36,15 @@ public abstract class TurnBasedGame extends Game
 	}
 	
 	/**
-	 * Get the correct Move object for a particular player. Default is to return
-	 * the same move for each Player. If private information is to be returned
-	 * for particular Players, use a Map of Players to Moves.
+	 * Gets the correct <code>Move</code> object with which to update a
+	 * particular player. Default behavior is to return the same move for each
+	 * Player, but implementations that require other behavior should override
+	 * this method.
+	 * 
+	 * @param player
+	 *            The player that will be sent the return result.
+	 * @return The <code>Move</code> object with which to update
+	 *         <code>player</code>.
 	 */
 	protected Move moveForPlayer(Player player)
 	{
@@ -42,10 +53,14 @@ public abstract class TurnBasedGame extends Game
 	
 	protected void init()
 	{
-		gameResult = new GameResult(activePlayers); 
-				// Default, should be overridden
+		gameResult = new GameResult(activePlayers);
 	}
 	
+	/**
+	 * This method sets the current player. If implementations need to override
+	 * this method, they should call <code>super.prePoll()</code> or be sure to
+	 * set the next player appropriately.
+	 */
 	protected void prePoll()
 	{
 		currentPlayer = nextPlayer();
@@ -63,9 +78,9 @@ public abstract class TurnBasedGame extends Game
 			
 	protected void updateEachPlayer(List<Player> players)
 	{
-		for(Player player : activePlayers) {
-			player.update(moveForPlayer(player)); 
-					// Default, can be overridden if need be.
+		for(Player player : activePlayers)
+		{
+			player.update(moveForPlayer(player));
 		}
 	}
 
