30fb849c15853cc3410b10b7ed95b985d9ca2a32
TheSheph <TheSheph@gmail.com>
2012-07-07 00:31:33 +0000

    Javadoc for Move and Player.

--- tourney/Player.java
+++ tourney/Player.java
@@ -1,40 +1,63 @@
 package tourney;
 
 /**
- * A Player could be used to implement a human player for interactive contexts.
- * (It might even be sensible to prepare such a HumanPlayer class for
- * interactive testing!) but it is intended to support a machine player for CPU
- * vs CPU competition.
+ * The <code>Player</code> interface is designed to allow artificial
+ * intelligence implementations to be designed for various games. This interface
+ * is flexible and can be used to develop players for all sorts of games. Its
+ * main purpose is to allow for machine implementations, but it can be used to
+ * allow for simple interactive implementations.
+ * <p>
+ * The contract for a <code>Player</code> is simple. Each player must keep its
+ * own version of the state of the game. The <code>update</code> method allows
+ * for the game to keep the player's state up to date. Then, when the
+ * <code>getMove</code> method is called by <code>Game</code>, the player simply
+ * evaluates the current game position and returns his intentions for his action
+ * encapsulated in a Move object.
+ * 
+ * @see Move
  */
 public interface Player {
 	/**
-	 * Demand a move from the player. No longer requires a state to be passed!
-	 * Each player keeps track of their own state of affairs and are responsible
-	 * for keeping it up to date.
+	 * Demands a move from this player. This player must evaluate the game
+	 * situation that it has been keeping, and return a legal <code>Move</code>
+	 * object for the game.
+	 * 
+	 * @return The actions this player wishes to perform, encapsulated in a Move
+	 *         object.
 	 */
 	Move getMove();
 
 	/**
-	 * A Player must have a unique identifier. This will allow stats to make
-	 * coherent reports. Tentative suggestion: ID should be composed of a game
-	 * code, player's DIC member name, and a serial number (to allow for
-	 * multiple entries)
+	 * Gets this player's identifier. A Player must have a unique identifier so
+	 * its statistics can be output and referenced. This will allow
+	 * <code>DataReader</code>s to make coherent reports.
+	 * <p>
+	 * Currently, IDs should take the form of:
+	 * <p>
+	 * <code>GameName_UserName_SubmissionNumber</code>
+	 * <p>
+	 * <b>GameName</b> - The name of the Game that this player has been designed
+	 * for.<br>
+	 * <b>UserName</b> - The name of the user submitting this player.<br>
+	 * <b>SubmissionNumber</b> - If the user is submitting more than one player,
+	 * append a submission number to the end of the ID. Otherwise, just use 1.
+	 * <br>
 	 * 
-	 * R.S: Suggested Form: GameName_DICName_Submission#_Instance#
-	 * 
-	 * GameName and DICName should be obvious.
-	 * 
-	 * Submission# is the number of their entry. (ie, TTT_Sheph_1 and
-	 * TTT_Sheph_2 are my two implementations.)
-	 * 
-	 * Instance# will be reserved for when we put TTT_Sheph_1 vs TTT_Sheph_1.
+	 * @return This player's unique identifier.
 	 */
 	String getID();
 
 	/**
-	 * Sends an update to the player so they may appropriately change
-	 * their game's state.
+	 * Sends an update to the player in the form of the necessary
+	 * <code>Move</code> object. The player should update their version of the
+	 * game state in this method and <i>only</i> in this method. The player
+	 * implementation should not try to update the state outside of this method,
+	 * nor should they call this method themselves. The game is responsible for
+	 * calling this method, even if it is with the player's own move.
+	 * 
+	 * @param move
+	 *            An encapsulation of the actions to take in order to change the
+	 *            state of the game appropriately.
 	 */
 	void update(Move move);
-	
 }
