74b97c641bc8365e013935ac530f6adf2b49c81a
jon.kiparsky <jon.kiparsky@gmail.com>
2012-08-02 04:05:45 +0000

    Generalized fix for issue 17, now all filters confirm that classes are not abstract or interface before returning true

--- tourney/loader/IsMachinePlayerFilter.java
+++ tourney/loader/IsMachinePlayerFilter.java
@@ -12,6 +12,8 @@ public class IsMachinePlayerFilter extends ClassFilter
 
 	public boolean filter(Class c)
 	{
+		if (cannotInstantiate(c))
+			return false;
 		if (MachinePlayer.class.isAssignableFrom(c))
 			return true;
 
