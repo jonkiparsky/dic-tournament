ba0a1d6465967897886c56befef4325c21ebcaf6
jon.kiparsky <jon.kiparsky@gmail.com>
2012-08-03 05:34:31 +0000

    Glitch in CountToNHuman meant it was both Human and Machine.

--- countToN/CountToNHuman.java
+++ countToN/CountToNHuman.java
@@ -10,7 +10,7 @@ import tourney.Player;
  * Recommended human implementation for the Count to N game.
  */
 public class CountToNHuman 
-			extends CountToNPlayer
+			extends Player
 			implements HumanPlayer 
 {		
 	static Scanner input = new Scanner(System.in);
